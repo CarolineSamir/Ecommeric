96b77d8a0d94d953fe1f473ad19a4f15d5289d3d
caroline <carolinesamir98@gmail.com>
2023-10-17 01:20:21 +0300

    1

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com/">
+
+    <!-- title -->
+    <title>Fruitkha</title>
+
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/png" href="{{asset('public/frutika')}}/img/favicon.png">
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="{{asset('public/frutika')}}/css/all.min.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="{{asset('public/frutika')}}/bootstrap/css/bootstrap.min.css">
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="{{asset('public/frutika')}}/css/owl.carousel.css">
+    <!-- magnific popup -->
+    <link rel="stylesheet" href="{{asset('public/frutika')}}/css/magnific-popup.css">
+    <!-- animate css -->
+    <link rel="stylesheet" href="{{asset('public/frutika')}}/css/animate.css">
+    <!-- mean menu css -->
+    <link rel="stylesheet" href="{{asset('public/frutika')}}/css/meanmenu.min.css">
+    <!-- main style -->
+    <link rel="stylesheet" href="{{asset('public/frutika')}}/css/main.css">
+    <!-- responsive -->
+    <link rel="stylesheet" href="{{asset('public/frutika')}}/css/responsive.css">
+
+</head>
